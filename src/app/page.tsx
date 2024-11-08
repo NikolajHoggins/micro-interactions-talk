bfa26ab0dac3f744d6346124312eeaebba684a3d
Hoggins <nikolajhjacobsen@gmail.com>
2024-11-08 16:28:21 +0100

    Added link to next example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import NavigationList from "./components/NavigationList";
 const items = [
   { title: "Buttons", href: "/examples/button" },
   { title: "Placeholder", href: "/examples/placeholder" },
+  { title: "ButtonGroup", href: "/examples/button-group" },
 ];
 
 export default function Home() {
