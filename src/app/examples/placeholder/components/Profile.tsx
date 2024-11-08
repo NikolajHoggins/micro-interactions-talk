c7d79c52fdb3b12edb855f86bd537705cc63c696
Hoggins <nikolajhjacobsen@gmail.com>
2024-11-08 16:27:51 +0100

    Added view profile button

--- src/app/examples/placeholder/components/Profile.tsx
+++ src/app/examples/placeholder/components/Profile.tsx
@@ -15,6 +15,7 @@ const LoadingProfile = () => {
       <div className="flex flex-col gap-2">
         <div className="h-8 w-48 bg-gray-200 animate-pulse" />
         <div className="h-4 w-96 bg-gray-200 animate-pulse" />
+        <div className="h-9 w-24 bg-gray-200 animate-pulse mt-2" />
       </div>
     </motion.div>
   );
@@ -46,6 +47,9 @@ const Profile = ({ member }: { member?: Member }) => {
       <div>
         <p className="text-2xl font-bold">{member.name}</p>
         <p className="text-base text-gray-500 w-96 text-wrap">{member.title}</p>
+        <button className="text-sm text-gray-500 border border-gray-300 rounded-sm px-1 py-2 mt-2 w-24 flex justify-center items-center">
+          View Profile
+        </button>
       </div>
     </motion.div>
   );
