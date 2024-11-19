36bf101d223331d659d791b2979024ead4ad01da
Hoggins <nikolajhjacobsen@gmail.com>
2024-11-19 10:05:35 +0100

    Some predefined widths for a little help

--- src/app/examples/placeholder/components/LoadingProfile.tsx
+++ src/app/examples/placeholder/components/LoadingProfile.tsx
@@ -3,3 +3,10 @@ import "./animation.css";
 const LoadingProfile = () => {};
 
 export default LoadingProfile;
+
+/*
+image = w-32 h-32
+name = h-8 w-48
+title = h-6 w-96
+button = h-10 w-24
+*/
--- src/app/examples/placeholder/components/Profile.tsx
+++ src/app/examples/placeholder/components/Profile.tsx
@@ -45,8 +45,10 @@ const Profile = ({ member }: { member?: Member }) => {
         />
       </div>
       <div>
-        <p className="text-2xl font-bold">{member.name}</p>
-        <p className="text-base text-gray-500 w-96 text-wrap">{member.title}</p>
+        <p className="text-2xl h-8 w-48 font-bold">{member.name}</p>
+        <p className="text-base text-gray-500 h-6 w-96 text-wrap">
+          {member.title}
+        </p>
         <button className="text-sm text-gray-500 border border-gray-300 rounded-sm px-1 py-2 mt-2 w-24 flex justify-center items-center">
           View Profile
         </button>
